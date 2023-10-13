478f24d51855d062e1a622bd68ca929286a0317a
andreygranenko <131700684+andreygranenko@users.noreply.github.com>
2023-10-13 13:17:04 +0300

    added all files

---
+++ js/modules/tabs.js
@@ -0,0 +1,40 @@
+function tabs(tabsSelector, tabsContentSelector, tabsParentSelector, activeClass) {
+    //Tabs
+    const tabs = document.querySelectorAll(tabsSelector),
+    tabsContent = document.querySelectorAll(tabsContentSelector),
+    tabsParent = document.querySelector(tabsParentSelector);
+  // console.log(tabsParent);
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.style.display = 'none';
+        });
+
+        tabs.forEach(tab => {
+            tab.classList.remove(activeClass);
+        });
+    }
+
+    function showTabContent (i = 0) {
+        tabsContent[i].style.display = 'block';
+        tabs[i].classList.add(activeClass);
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if (target && target.classList.contains(tabsSelector.slice(1))) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+
+}
+
+export default tabs;
